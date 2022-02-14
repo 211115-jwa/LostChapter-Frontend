bd718d0e7a507a91cebd6a4905dc02051e494539
ricky23i <rduenas2@illinois.edu>
2022-02-14 15:56:21 -0600

    constant routing

--- src/app/services/search-products.service.ts
+++ src/app/services/search-products.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchProductsService {
-
+  private host = environment.hostURL;
   constructor(private http: HttpClient) { }
 
   getAllGenre(){
-    return this.http.get(`http://localhost:8081/book/genre`, {
+    return this.http.get(`${this.host}/book/genre`, {
       //`http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/books/search/${searchItem}`, {
       observe: 'response'
     })
@@ -17,28 +18,28 @@ export class SearchProductsService {
   }
 
   getAllBooks(){
-    return this.http.get(`http://localhost:8081/book`, {
+    return this.http.get(`${this.host}/book`, {
       //`http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/books/search/${searchItem}`, {
       observe: 'response'
     })
   }
 
   getSearchResult(searchItem: string){
-    return this.http.get(`http://localhost:8081/book/search/${searchItem}`, {
+    return this.http.get(`${this.host}/book/search/${searchItem}`, {
       //`http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/books/search/${searchItem}`, {
         observe: 'response'
     })
   }
 
   getSearchByGenre(genre: string){
-    return this.http.get(`http://localhost:8081/book/genre/${genre}`, {
+    return this.http.get(`${this.host}/book/genre/${genre}`, {
       //`http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/books/genre/${genreId}`, {
       observe: 'response'
     })
   }
 
   getBookBySales(){
-    return this.http.get(`http://localhost:8081/book/books/sales`, {
+    return this.http.get(`${this.host}/book/books/sales`, {
       //`http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/books/sales`, {
       observe: 'response'
     })
@@ -48,7 +49,7 @@ export class SearchProductsService {
     let parameter = new HttpParams();
     parameter = parameter.append('bookId', productId);
     parameter = parameter.append('quantityToBuy', quantity);
-    return this.http.post(`http://localhost:8081/users/{userId}/cart`, {},
+    return this.http.post(`${this.host}/users/{userId}/cart`, {},
       //`http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/users/${userId}/cart`, {},
       {
         "params": parameter,
@@ -58,14 +59,14 @@ export class SearchProductsService {
   }
 
   getBookById(bookId: number){
-    return this.http.get(`http://localhost:8081/book/${bookId}`, {
+    return this.http.get(`${this.host}book/${bookId}`, {
       //`http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/books/${bookId}`, {
       observe: 'response'
     })
   }
 
   getFeaturedBooks(){
-    return this.http.get(`http://localhost:8081/book/featured`, {
+    return this.http.get(`${this.host}/book/featured`, {
       //`http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/books/featured`, {
       observe: 'response'
     })
